1d6e7c4917af64d44626400a600569ce71d8fc82
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-24 07:04:07 +0100

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates a file at /tmp
 file { '/tmp/school':
-    ensure => 'file',
-    owner => 'www-data',
-    group => 'www-data',
-    mode => '0744',
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
     content => 'I love Puppet',
 }
