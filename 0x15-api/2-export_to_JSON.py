18367ab6562d5d1c8217d0f01434aa4720f346a6
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-07-27 12:35:15 +0100

    Create 2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""exporting data to a json file"""
+from sys import argv
+import json
+import requests
+
+
+if __name__ == "__main__":
+    employee_id = argv[1]
+    api_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+
+    # get username from API
+    response = requests.get(api_url)
+    username = response.json().get('username')
+
+    # get todo list
+    todo_url = api_url + "/todos"
+    response = requests.get(todo_url)
+    tasks = response.json()
+
+    json_dict = {employee_id: []}
+
+    for task in tasks:
+        json_dict[employee_id].append({
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": username
+            })
+    
+    file_name = employee_id + '.json'
+
+    with open(file_name, 'w') as file:
+        json.dump(json_dict, file)
