00de4320e288a6fc5220213779019bac15eced60
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-07-27 13:03:03 +0100

    Create 3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""exporting data to a json file"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    api_url = "https://jsonplaceholder.typicode.com/users"
+
+    # get users from API
+    response = requests.get(api_url)
+    users = response.json()
+
+    # putting the result of the get request in a dict
+    dict = {}
+
+    # get todo list
+    for user in users:
+        user_id = user.get('id')
+        url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+        response = requests.get(url)
+        tasks = response.json()
+
+        dict[user_id] = []
+
+        for task in tasks:
+            dict[user_id].append({
+                "task": task.get('title'),
+                "completed": task.get('completed'),
+                "username": task.get('username')
+            })
+    
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(dict, file)
+    
