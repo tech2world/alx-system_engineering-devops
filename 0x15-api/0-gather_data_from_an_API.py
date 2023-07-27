b326734e3172105866764fe20e3a9262ff2de842
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-07-27 14:08:40 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,25 +13,18 @@ if __name__ == "__main__":
         # Sending a get request to API for employee info
         response = requests.get(api_url)
 
-        if response.status_code == 200:
-            employee_data = response.json()
-            employee_name = employee_data.get('name', 'Unknown Employee')
+    
+        employee_data = response.json()
+        employee_name = employee_data.get('name', 'Unknown Employee')
 
-            # Sending a get request to API for employee's TODO tasks
-            todo_url = api_url + '/todos'
-            response = requests.get(todo_url)
+        # Sending a get request to API for employee's TODO tasks
+        todo_url = api_url + '/todos'
+        response = requests.get(todo_url)
 
-            if response.status_code == 200:
-                todo_data = response.json()
-                completed_tasks = [task for task in todo_data if task.get('completed')]
+        todo_data = response.json()
+        completed_tasks = [task for task in todo_data if task.get('completed')]
 
-                print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{len(todo_data)}):")
+        print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{len(todo_data)}):")
 
-                for task in completed_tasks:
-                    print("\t{}".format(task.get('title')))
-            else:
-                print(f"Error: API request for TODO tasks failed with status code {response.status_code}")
-                exit(1)
-        else:
-            print(f"Error: API request for employee info failed with status code {response.status_code}")
-            exit(1)
+        for task in completed_tasks:
+            print("\t{}".format(task.get('title')))
