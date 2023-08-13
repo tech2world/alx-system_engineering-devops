6d641fb378ee08aa1fd2f326575908c12ad22bf7
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-08-14 00:36:26 +0100

    web debugging

---
+++ 0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# puppet file to replace a line on the server
+# change phpp to php
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => "/usr/local/bin/:/bin/"
+}
