4c3a2816f798a664172f5e1bcfc414804f3daa9e
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-08-14 01:00:58 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,6 @@
 # change phpp to php
 
 exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => "/usr/local/bin/:/bin/"
+    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+    path    => "/usr/local/bin/:/bin/"
 }
